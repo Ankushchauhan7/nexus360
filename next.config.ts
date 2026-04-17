e4153b5cb00e3ea9e540601180f991ebc32c283a
Ankushchauhan7 <ankushchauhan10998@gmail.com>
2026-04-17 23:32:28 +0530

    kkk

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,41 @@
 import type { NextConfig } from "next";
 
+const securityHeaders = [
+  { key: "X-DNS-Prefetch-Control", value: "on" },
+  { key: "X-Frame-Options", value: "SAMEORIGIN" },
+  { key: "X-Content-Type-Options", value: "nosniff" },
+  { key: "Referrer-Policy", value: "origin-when-cross-origin" },
+  {
+    key: "Permissions-Policy",
+    value: "camera=(), microphone=(), geolocation=()",
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+];
+
+
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
+
   images: {
     formats: ["image/avif", "image/webp"],
   },
+   allowedDevOrigins: ['192.168.1.14'],
 
-  allowedDevOrigins: ["192.168.1.14"], 
+  compress: true,
+
+  poweredByHeader: false,
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
